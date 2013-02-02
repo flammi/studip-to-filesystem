d96dd3f727c7d159b1b092de96be174bd9099937
Fabian Franzen <flammi@watteimdocht.de>
2013-02-02 20:05:13 +0100

    Refactoring - Use classes
    Encapsulated the Studip Interface behind  a class

--- studip.rb
+++ studip.rb
@@ -2,35 +2,48 @@ require 'nokogiri'
 require 'mechanize'
 require 'highline/import'
 
+class Course
+	attr_reader :name
+	def initialize(name)
+		@name = name
+	end
+end
+
+class Studip
+	def initialize(username, password)
+		@fake_browser = Mechanize.new
+		p = @fake_browser.get "http://elearning.uni-bremen.de/"
+		login_form = p.form_with :name => "login"
+		login_form.field_with(:name => "loginname").value = username
+		login_form.field_with(:name => "password").value = password
+		@fake_browser.submit login_form
+	end
+	def list_courses()
+		veranstaltungen = @fake_browser.get "http://elearning.uni-bremen.de/meine_seminare.php"
+		doc = Nokogiri::HTML(veranstaltungen.body)
+
+		res = Array.new
+
+		doc.css('a[href*="seminar_main.php"]').each do |link| 
+			c = link.content.strip
+			if c != ""
+				res << Course.new(c)
+			end
+		end
+		res
+	end
+end
+
+
+
 puts "Studip Notifications download"
-
-#From stackoverflow...
-def get_password(prompt="Enter password:")
-	ask(prompt) {|q| q.echo = false}
-end
-
-def get_username()
-	ask("Username:")
-end
-
-fake_browser = Mechanize.new
-p = fake_browser.get "http://elearning.uni-bremen.de/"
-login_form = p.form_with :name => "login"
-login_form.field_with(:name => "loginname").value = get_username()
-login_form.field_with(:name => "password").value = get_password()
+user = ask("Username:") {|q| q.echo = true}
+pw = ask("Password:") {|q| q.echo = false}
 
 puts "Trying login..."
-
-fake_browser.submit login_form
+studip = Studip.new(user, pw)
 
 puts "Downloading courses list..."
-veranstaltungen = fake_browser.get "http://elearning.uni-bremen.de/meine_seminare.php"
-
-doc = Nokogiri::HTML(veranstaltungen.body)
-
-doc.css('a[href*="seminar_main.php"]').each do |link| 
-	c = link.content.strip
-	if c != ""
-		puts c
-	end
+(studip.list_courses).each do |course|
+	puts course.name
 end
