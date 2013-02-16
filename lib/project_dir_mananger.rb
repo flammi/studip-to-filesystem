3d81ab074b986431eae62ca9f2ef20cecb0f3a24
StateOfTheArt <malte@Meta.(none)>
2013-02-16 15:00:03 +0100

    Bugfix: "Kurs ohne Dateien-Sektion", Formatierung

--- lib/project_dir_mananger.rb
+++ lib/project_dir_mananger.rb
@@ -1,39 +1,42 @@
 class ProjectDirManager
-	def initialize()
-		@project_dirs = Hash.new
-	end
-	def import_dirs(wd)
-		Dir.glob("#{wd}/**/.studipdl").each do |file|
-			f = File.new(file, "r")
-			cid = f.readline
-			course_dir = File.dirname(file)
-			@project_dirs[cid] = course_dir
-		end
-		@project_dirs.size
-	end
-	def clear()
-		@project_dirs.clear
-	end
-	def clean_up_course_name(course_name)
-		#Klammern aus dem Namen entfernen
-		clean_up_name = course_name.gsub(/\([^\)]*\)/, "").strip
-		#Slashes entfernen
-		clean_up_name.gsub("/", " ")
-	end
-	def dir_for(course_name, cid)
-		if not @project_dirs[cid]
-			#Create dir
-			newDir = clean_up_course_name(course_name)
-			if not File.directory? newDir
-				Dir::mkdir(newDir)
-			end
+  def initialize()
+    @project_dirs = Hash.new
+  end
 
-			#Create info file
-			File.open("#{newDir}/.studipdl", "w") do |f|
-				f.write(cid)
-			end
-			@project_dirs[cid] = newDir
-		end
-		@project_dirs[cid]
-	end
+  def import_dirs(wd)
+    Dir.glob("#{wd}/**/.studipdl").each do |file|
+      f = File.new(file, "r")
+      cid = f.readline
+      course_dir = File.dirname(file)
+      @project_dirs[cid] = course_dir
+    end
+    @project_dirs.size
+  end
+
+  def clear()
+    @project_dirs.clear
+  end
+  def clean_up_course_name(course_name)
+    #Klammern aus dem Namen entfernen
+    clean_up_name = course_name.gsub(/\([^\)]*\)/, "").strip
+    #Slashes entfernen
+    clean_up_name.gsub("/", " ")
+  end
+
+  def dir_for(course_name, cid)
+    if not @project_dirs[cid]
+      #Create dir
+      newDir = clean_up_course_name(course_name)
+      if not File.directory? newDir
+        Dir::mkdir(newDir)
+      end
+
+      #Create info file
+      File.open("#{newDir}/.studipdl", "w") do |f|
+        f.write(cid)
+      end
+      @project_dirs[cid] = newDir
+    end
+    @project_dirs[cid]
+  end
 end
--- lib/studip.rb
+++ lib/studip.rb
@@ -3,73 +3,84 @@ require 'mechanize'
 require 'debugger'
 
 class Course
-	attr_reader :name, :files_new, :cid
-	def initialize(browser, node, cid)
-		@browser = browser
-		@name = node.content.strip
-		@files_new = false
-		@cid = cid
-		node.parent.parent.css("img").each do |img|
-			attribute = img.attribute("src").value
-			if attribute.index("red") and attribute.index("file")
-				@files_new = true
-			end
-		end
-	end
+  attr_reader :name, :files_new, :cid
 
-	def files()
-		filepage = @browser.get "http://elearning.uni-bremen.de/folder.php?cid=#{@cid}&cmd=all"
-		doc = Nokogiri::HTML(filepage.body)
+  def initialize(browser, node, cid)
+    @browser = browser
+    @name = node.content.strip
+    @files_new = false
+    @cid = cid
+    node.parent.parent.css("img").each do |img|
+      attribute = img.attribute("src").value
+      if attribute.index("red") and attribute.index("file")
+        @files_new = true
+      end
+    end
+  end
 
-		doc.css('a.extern').map do |x|
-			filename = /.*[&?]file_name=([^&?]+).*/.match(x.attribute("href").value)[1]
-			link = x.attribute("href").value
-			StudipFile.new @browser, filename, link
-		end
-	end
+  def files()
+    begin
+      filepage = @browser.get "http://elearning.uni-bremen.de/folder.php?cid=#{@cid}&cmd=all"
+    rescue
+      puts "Couldn't get files"
+      return
+    end
+    doc = Nokogiri::HTML(filepage.body)
+
+    doc.css('a.extern').map do |x|
+      filename = /.*[&?]file_name=([^&?]+).*/.match(x.attribute("href").value)[1]
+      link = x.attribute("href").value
+      StudipFile.new @browser, filename, link
+    end
+  end
 end
 
 class StudipFile
-	attr_reader :name, :link
-	def initialize(browser, file_name, link)
-		@browser = browser
-		@name = file_name
-		@link = link
-	end
-	def download_to(location)
-		file = @browser.get(@link)
-		if file.respond_to?("save_as")
-			file.save_as location
-		else
-			puts "Error: Skipping file #{file}"
-		end
-	end
+  attr_reader :name, :link
+
+  def initialize(browser, file_name, link)
+    @browser = browser
+    @name = file_name
+    @link = link
+  end
+
+  def download_to(location)
+    file = @browser.get(@link)
+    if file.respond_to?("save_as")
+      file.save_as location
+    else
+      puts "Error: Skipping file #{file}"
+    end
+  end
+
 end
 
 class Studip
-	def initialize(username, password)
-		@fake_browser = Mechanize.new
-		p = @fake_browser.get "http://elearning.uni-bremen.de/"
-		login_form = p.form_with :name => "login"
-		login_form.field_with(:name => "loginname").value = username
-		login_form.field_with(:name => "password").value = password
-		@fake_browser.submit login_form
-	end
-	def list_courses()
-		veranstaltungen = @fake_browser.get "http://elearning.uni-bremen.de/meine_seminare.php"
-		doc = Nokogiri::HTML(veranstaltungen.body)
 
-		res = Array.new
+  def initialize(url, username, password)
+    @fake_browser = Mechanize.new
+    p = @fake_browser.get url
+    login_form = p.form_with :name => "login"
+    login_form.field_with(:name => "loginname").value = username
+    login_form.field_with(:name => "password").value = password
+    @fake_browser.submit login_form
+  end
 
-		doc.css('a[href*="seminar_main.php"]').each do |link| 
-			c = link.content.strip
-			link_href = link.attribute("href").value
-			cid = /.*[?&]auswahl=([^&?]+).*/.match(link_href)[1]
-			if c != ""
-				res << Course.new(@fake_browser, link, cid)
-			end
-		end
-		res
-	end
+  def list_courses()
+    veranstaltungen = @fake_browser.get "http://elearning.uni-bremen.de/meine_seminare.php"
+    doc = Nokogiri::HTML(veranstaltungen.body)
+
+    res = Array.new
+
+    doc.css('a[href*="seminar_main.php"]').each do |link| 
+      c = link.content.strip
+      link_href = link.attribute("href").value
+      cid = /.*[?&]auswahl=([^&?]+).*/.match(link_href)[1]
+      if c != ""
+        res << Course.new(@fake_browser, link, cid)
+      end
+    end
+    res
+  end
 end
 
